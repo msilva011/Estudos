afc16d7ac46bf26e51c9d7cdaf794219860f8183
Mayara Silva <62021658+msilva011@users.noreply.github.com>
2023-03-31 13:39:35 -0300

    Delete Voltando ao Básico/POO_Java/Java/POO/Main directory

--- Voltando ao Básico/POO_Java/Java/POO/Main/src/App.java
+++
@@ -1,7 +0,0 @@
-public class App {
-    public static void main(String[] args){
-     Caneta caneta1 = new Caneta("Mayara","vermelha",               0.7f,true);
-      caneta1.status();
-    }
- }
- 
